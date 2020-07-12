d320788cfd32f3535827bcee1d0b4188866104d7
praveen-me <praveenkrsaini0@gmail.com>
2020-07-12 18:08:52 +0530

    Added initial files

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const webpack = require("webpack");
+const helpers = require("./webpack.helpers.js");
+
+const fileSystem = helpers.generateFS(`${__dirname}/src/actions`, "workerB");
+
+const entryFiles = helpers.generateEntryPaths(fileSystem.children);
+
+const entryPaths = helpers
+  .getFiles(entryFiles, ".ts")
+  .map((file) => file.replace(".ts", ""))
+  .filter((f) => f.split(".")[1] !== "json");
+
+const metaFiles = helpers.getFiles(entryFiles, ".json");
+
+module.exports = {
+  entry: entryPaths.reduce((result, entryPath) => {
+    result[entryPath] = `./src/actions/${entryPath}.ts`;
+    return result;
+  }, {}),
+  output: {
+    path: path.resolve(__dirname, "gitlab"),
+    filename: "[name].js",
+    globalObject: "this",
+    libraryTarget: "umd",
+    library: "main",
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        query: { presets: ["@babel/preset-env"] },
+      },
+    ],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: metaFiles.map((metaFile) => {
+        const from = `./src/actions${metaFile}`;
+        const to = `./${metaFile}`;
+
+        return {
+          from,
+          to,
+        };
+      }),
+      options: {
+        concurrency: 100,
+      },
+    }),
+    new webpack.DefinePlugin(helpers.envKeys),
+  ],
+};
