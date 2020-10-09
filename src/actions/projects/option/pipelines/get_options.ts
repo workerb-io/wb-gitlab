5d0e07106544785020dcca23da7043a383f6cac1
Saurabh Garg <sgarg@workerb.io>
2020-10-09 11:06:16 -0700

    Support for opening a pipeline

--- src/actions/projects/option/pipelines/get_options.ts
+++ src/actions/projects/option/pipelines/get_options.ts
@@ -5,7 +5,7 @@ function parseISOString(s: string) {
 	var b = s.split(/\D+/)
 	return new Date(
 		Date.UTC(Number(b[0]), Number(b[1]) - 1, Number(b[2]), Number(b[3]), Number(b[4]), Number(b[5]), Number(b[6]))
-	)
+	).toUTCString()
 }
 
 export default () => {
