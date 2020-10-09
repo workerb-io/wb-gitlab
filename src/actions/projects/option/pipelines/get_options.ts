c8b6089de43690cbf247fdd13ae9a2b3bcf9f1d0
Saurabh Garg <sgarg@workerb.io>
2020-10-09 10:47:16 -0700

    Support for opening a pipeline

--- src/actions/projects/option/pipelines/get_options.ts
+++ src/actions/projects/option/pipelines/get_options.ts
@@ -1,6 +1,13 @@
 import { decodeApiResponse, handleErrors } from '../../../../utils/helper'
 import { getAllPipelines } from '../../../../utils/api'
 
+function parseISOString(s: string) {
+	var b = s.split(/\D+/)
+	return new Date(
+		Date.UTC(Number(b[0]), Number(b[1]) - 1, Number(b[2]), Number(b[3]), Number(b[4]), Number(b[5]), Number(b[6]))
+	)
+}
+
 export default () => {
 	if (options?.projects) {
 		const { id: projectId } = options.projects
@@ -11,11 +18,24 @@ export default () => {
 			return {}
 		}
 
-		const pipelines = result.response.map(({ web_url, id }: { web_url: string; id: number }) => ({
-			id,
-			name: `Pipeline: #${id}`,
-			html_url: web_url,
-		}))
+		const pipelines = result.response.map(
+			({
+				web_url,
+				id,
+				status,
+				created_at,
+			}: {
+				web_url: string
+				id: number
+				status: string
+				created_at: string
+			}) => ({
+				id,
+				name: `Pipeline: #${id}`,
+				html_url: web_url,
+				description: `Created At: ${parseISOString(created_at)}. Status: ${status}`,
+			})
+		)
 
 		// eslint-disable-next-line consistent-return
 		return JSON.stringify({
