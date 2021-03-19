2d5a232b25bf78ecc72938272feff7ccc9646f6f
Puneet Aggarwal <puneet@Puneets-MacBook-Pro.local>
2021-03-19 11:50:49 +0530

    Added logout action

---
+++ src/actions/logout.ts
@@ -0,0 +1,10 @@
+// @description Logout from gitlab automation package
+
+setVars([
+	{
+		name: 'GITLAB_PERSONAL_TOKEN',
+		value: '',
+	},
+], { local: true });
+notify('Access token removed successfully', 'success', 3000);
+reIndex();
