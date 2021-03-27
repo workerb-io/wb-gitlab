2a98919f4b259c296962d154972b9be5b53a2d73
puneet222 <aggarwal.puneet222@gmail.com>
2021-03-27 12:30:44 +0530

    handle user logout scenario

--- src/actions/setup.ts
+++ src/actions/setup.ts
@@ -2,41 +2,53 @@
 let token = null
 
 if (args[0]) {
-	token = args[0]
+	token = args[0];
+	setAccessToken(token);
 } else {
 	open('https://gitlab.com/profile/personal_access_tokens')
-	const tokenName = `workerb-${new Date().getTime()}`
 
-	type(tokenName, 'Name', { method: 'by_label' })
+	const currentURL = readURL();
 
-	click('api', {})
-	click('read_user', {})
-	click('read_api', {})
-	click('read_repository', {})
-	click('write_repository', {})
-	click('read_registry', {})
-	click('write_registry', {})
+	if (currentURL.indexOf("sign_in") === -1) {
+		// user logged in
+		const tokenName = `workerb-${new Date().getTime()}`
 
-	//@ts-ignore
-	submit('Name', {
+		type(tokenName, 'Name', { method: 'by_label' })
+
+		click('api', {})
+		click('read_user', {})
+		click('read_api', {})
+		click('read_repository', {})
+		click('write_repository', {})
+		click('read_registry', {})
+		click('write_registry', {})
+
 		//@ts-ignore
-		method: 'by_label',
-		//@ts-ignore
-		expectReload: true,
-	})
-
-	token = read('#created-personal-access-token', { method: 'by_query_selector' })
+		submit('Name', {
+			//@ts-ignore
+			method: 'by_label',
+			//@ts-ignore
+			expectReload: true,
+		})
+		token = read('#created-personal-access-token', { method: 'by_query_selector' });
+		setAccessToken(token);
+	} else {
+		// user not logged in
+		notify("Please login into your Gitlab account", "error", 3000)
+	}
 }
 
-if (!token) {
-	notify('Failed to save the auth token.', 'error', 3000)
-} else {
-	setVars([
-		{
-			name: 'GITLAB_PERSONAL_TOKEN',
-			value: token,
-		},
-	], { local: true })
-	notify('Access token added successfully.', 'success', 3000)
-	reIndex([])
+function setAccessToken(token: string) {
+	if (!token) {
+		notify('Failed to save the auth token.', 'error', 3000)
+	} else {
+		setVars([
+			{
+				name: 'GITLAB_PERSONAL_TOKEN',
+				value: token,
+			},
+		], { local: true })
+		notify('Access token added successfully.', 'success', 3000)
+		reIndex([])
+	}
 }
